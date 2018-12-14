0f8a9bde855a37b49f0dd3e6de0c9c87807a0ff7
YZakem <38890509+YZakem@users.noreply.github.com>
2018-12-14 12:21:35 -0500

    Create Station.java

---
+++ Station.java
@@ -0,0 +1,34 @@
+public class Station {
+
+	final private double parkingCost;
+	final private double costToUnionStation;
+	final private String name;
+
+	public Station(String name, double parkingCost, double costToUnionStation) {
+		this.name = name;
+		if (parkingCost < 0) {
+			throw new IllegalArgumentException("You don't get paid to park!");
+		}
+		this.parkingCost = parkingCost;
+		this.costToUnionStation = costToUnionStation;	
+	}
+	
+	public double rideCost(boolean roundTrip) {
+		double total;
+		if (roundTrip) {
+			total = this.costToUnionStation * 2 + this.parkingCost;
+		}
+		else {
+			total = this.costToUnionStation + this.parkingCost;
+		}
+		return total;
+	}
+	
+	
+	public static void main(String[] args) {
+		Station forestGlen = new Station("Forest Glen", 3.30, 4.45);
+		System.out.println("The cost from " + forestGlen.name + " is $" + forestGlen.rideCost(true));
+
+	}
+
+}
